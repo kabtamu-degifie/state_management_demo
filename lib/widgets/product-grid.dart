e1cfe700630f59e507c7467801e91d11e5d9329f
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-05-09 23:43:23 +0300

    provider with value

--- lib/widgets/product-grid.dart
+++ lib/widgets/product-grid.dart
@@ -16,8 +16,10 @@ class ProductGrid extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-          create: (context) => products[i], child: ProductItem()),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+          // create: (context) => products[i],
+          value: products[i],
+          child: ProductItem()),
       itemCount: products.length,
     );
   }
