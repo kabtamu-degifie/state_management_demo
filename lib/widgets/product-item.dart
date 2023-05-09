26aa1af0bd3ed7c403c8324d289206865e289b35
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-05-09 23:54:08 +0300

    Consumer

--- lib/widgets/product-item.dart
+++ lib/widgets/product-item.dart
@@ -10,29 +10,31 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
-                color: Colors.deepOrangeAccent,
-                onPressed: () {
-                  product.toggleFavorite();
-                },
-                icon: Icon(product.isFavorite
-                    ? Icons.favorite
-                    : Icons.favorite_border)),
-            trailing: IconButton(
-                color: Colors.deepOrangeAccent,
-                onPressed: () {},
-                icon: const Icon(Icons.add_shopping_cart)),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            )),
-        child: Image.network(product.imageUrl),
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (context, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+              backgroundColor: Colors.black87,
+              leading: IconButton(
+                  color: Colors.deepOrangeAccent,
+                  onPressed: () {
+                    product.toggleFavorite();
+                  },
+                  icon: Icon(product.isFavorite
+                      ? Icons.favorite
+                      : Icons.favorite_border)),
+              trailing: IconButton(
+                  color: Colors.deepOrangeAccent,
+                  onPressed: () {},
+                  icon: const Icon(Icons.add_shopping_cart)),
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              )),
+          child: Image.network(product.imageUrl),
+        ),
       ),
     );
   }
